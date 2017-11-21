de8e51c0a44a732fb9c109c95fe2f47aed6fc789
Anton.Dorofejev <zarthra@gmail.com>
2017-11-21 19:55:46 +0200

    Update main.py
    2 variables added and printed

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 print("Simple repo")
 print("Additional repositories may be preplanned.")
 
+
 statement = "This is a statement"
+
+intVar = 3
+chVar = "Something"
+print(str(intVar) + " " + chVar)
+
